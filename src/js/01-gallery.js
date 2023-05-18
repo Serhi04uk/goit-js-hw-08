560377d282ee9a506acebc2708bc50fc4e8e2eb4
Serhi04uk <Sergey.willian17@gmail.com>
2023-05-18 18:10:02 +0200

    ++

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,7 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
-
-// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallaryEl = document.querySelector('.gallery');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const videoIframe = document.getElementById('vimeo-player');
+
+const player = new Player(videoIframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime !== null) {
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {
+      console.log(`Значение времени из localeStorage: ${seconds}`);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log(
+            'Вызвано некорректным временем в setCurrentTime и установило время 0'
+          );
+          break;
+
+        default:
+          console.log('ошибки с другими причинами');
+          break;
+      }
+    });
+}
